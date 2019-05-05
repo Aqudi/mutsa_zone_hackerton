aa5a113b41ec33abe428e36833e74c99f8af63ff
Ju Yeong <2jy22@naver.com>
2019-05-05 10:23:23 +0000

    navbar, main, list, views...

--- project/meetings/urls.py
+++ project/meetings/urls.py
@@ -3,12 +3,14 @@ from . import views
 
 app_name="meetings"
 urlpatterns = [
-    # path('list/', views.list, name="list"),
+    path('list/', views.list, name="list"),
     # path('<int:id>/', views.read, name="read"),
-    # path('create/', views.create, name="create"),
+    path('create/', views.create, name="create"),
     # path('verification/', views.verification, name="verification"),
     # path('edit/<int:id>/', views.edit, name="edit"),
+    # path('update/<int:id>/', views.update, name="update"),
     # path('search_result/', views.search_result, name="search_result"),
+    # path('join_page/<int:id>/', views.join_page, name="join_page"),
     # path('join/<int:id>/', views.join, name="join"),
     # path('participants/', views.participants, name="participants"),
     # path('joined_meetings/', views.joined_meetings, name="joined_meetings"),
--- project/meetings/views.py
+++ project/meetings/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Meeting, Participant
 
 # Create your views here.
+# 모임 개설
+def create(request):
+    if request.method == "POST":
+        category = request.POST.get('category')
+        title = request.POST.get('title')
+        writer = request.POST.get('writier')
+        min_number = request.POST.get('min_number')
+        max_number = request.POST.get('max_number')
+        meeting_time = request.POST.get('meeting_time')
+        meeting_place = request.POST.get('meeting_place')
+        description = request.POST.get('description')
+        writer_email = request.POST.get('writer_email')
+        password = request.POST.get('password')
+        
+        meeting = Meeting(category = category, title = title, wrtier = writer, min_number = min_number, max_number = max_number, meeting_time = meeting_time, meeting_place = meeting_place, description = description, writer_email = writer_email, password=password)
+        meeting.save()
+        return redirect('list')
+        
+    return render(request, 'meetings/create.html')
+
+
+# 모임 목록
+def list(request):
+    meetings = Meeting.objects.all()
+    return render(request, 'meetings/list.html', {'meetings':meetings})
+    
+# 모임 참여 페이지로 넘어가는 것 - id 받아서 다시 넘겨야 함
+def join_page(request, id):
+    meeting = Meeting.objects.get(pk=id)
+    title = meeting.title
+    return render(request, 'meetings/join.html', {'title': title})
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+
+def join(request, id):
+    if request.method == "POST":
+        
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        school = request.POST.get('school')
+        motive = request.POST.get('motive')
+        
+        meeting = Meeting(name = name, email = email, school = school, motive = motive)
+        meeting.save()
+        return redirect('meetings:list')
+        
+
+# 모임 수정 페이지 띄우기
+def edit(request, id):
+        meeting = get_object_or_404(Meeting, pk=id)
+        return render(request, 'meetings/edit.html', {'meeting':meeting})
+
+
+# 모임 수정
+def update(request, id):
+    if request.method == "POST":
+        meeting = get_object_or_404(Meeting, pk=id)
+        category = request.POST.get('category')
+        title = request.POST.get('title')
+        writer = request.POST.get('writier')
+        min_number = request.POST.get('min_number')
+        max_number = request.POST.get('max_number')
+        meeting_time = request.POST.get('meeting_time')
+        meeting_place = request.POST.get('meeting_place')
+        description = request.POST.get('description')
+        writer_email = request.POST.get('writer_email')
+
+        meeting.category = category
+        meeting.title = title
+        meeting.writer = writer
+        meeting.min_number = min_number
+        meeting.max_number = max_number
+        meeting.meeting_time = meeting_time
+        meeting.meeting_place = meeting_place
+        meeting.description = description
+        meeting.writer_email = writer_email
+
+        meeting.save()
+
+        return redirect('meetings:read', meeting.pk)
+
+# 모임 삭제하기
+def delete(request, id):
+    if request.method == "POST":
+        meeting = Meeting.objects.get(pk=id)
+        meeting.delete()
+        return redirect('meeting:list')
+        
+        
+# 모임 상세보기
+def read(request, id):
+    meeting = get_object_or_404(Meeting, pk=id)
+    default_view_count = meeting.view_count
+    meeting.view_count = default_view_count + 1
+    meeting.save()
+    return render(request, 'meeting/read.html', {'meeting': meeting})
+
+
+def verification(request):
+    return render(request, 'meetings/verification.html')
