de6d703712cabe36774783d264c7a58fa034621e
Ju Yeong <2jy22@naver.com>
2019-05-05 06:54:07 +0000

    add urlpatterns

--- project/meetings/urls.py
+++ project/meetings/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 app_name="meetings"
 urlpatterns = [
-    
+    # path('list/', views.list, name="list"),
+    # path('<int:id>/', views.read, name="read"),
+    # path('create/', views.create, name="create"),
+    # path('verification/', views.verification, name="verification"),
+    # path('edit/<int:id>/', views.edit, name="edit"),
+    # path('search_result/', views.search_result, name="search_result"),
+    # path('join/<int:id>/', views.join, name="join"),
+    # path('participants/', views.participants, name="participants"),
+    # path('joined_meetings/', views.joined_meetings, name="joined_meetings"),
+    # path('joined_list/', views.joined_list, name="joined_list"),
 ]
