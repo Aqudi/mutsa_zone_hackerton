371557ac7f2eb75a7712174be1228461a93465f3
Ju Yeong <2jy22@naver.com>
2019-05-05 07:23:46 +0000

    model migrate

--- project/meetings/models.py
+++ project/meetings/models.py
@@ -1,36 +1,36 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here.
 class Meeting(models.Model):
     objects = models.Manager()
 
-    category = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    writer = models.CharField(max_length=200)
-    min_number = models.IntegerField()
-    max_number = models.IntegerField()
-    current_number = models.IntegerField(default=0)
-    meeting_time = models.CharField(max_length=200)
-    meeting_place = models.CharField(max_length=200)
-    description = models.TextField()
-    writer_email = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
+    category = models.CharField(max_length=200) # 카테고리
+    title = models.CharField(max_length=200) # 제목
+    writer = models.CharField(max_length=200) # 개설자
+    min_number = models.IntegerField() # 최소인원수
+    max_number = models.IntegerField() # 최대인원수
+    current_number = models.IntegerField(default=0) # 현재인원수
+    meeting_time = models.CharField(max_length=200) # 시간
+    meeting_place = models.CharField(max_length=200) # 장소
+    description = models.TextField() # 설명
+    writer_email = models.CharField(max_length=200) # 개설자 이메일
+    password = models.CharField(max_length=200) # 비밀번호
 
-    view_count = models.IntegerField(default=0)
+    view_count = models.IntegerField(default=0) # 조회수
 
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Participant(models.Model):
     objects = models.Manager()
 
-    name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    school = models.CharField(max_length=200)
-    motive = models.TextField()
+    name = models.CharField(max_length=200) # 참가자 이름
+    email = models.CharField(max_length=200) # 참가자 이메일
+    school = models.CharField(max_length=200) # 참가자 소속학교
+    motive = models.TextField() # 참여동기
 
     meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
